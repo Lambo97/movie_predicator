453a19229f90e87f27dc923c365f992c2e888fcb
Lambo97 <lamborellemaxime@MacBookMaxime.local>
2018-12-12 17:36:42 +0100

    Delete -1 and value over 5

---
+++ cheat.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import time
+
+def load_from_csv(path, delimiter=','):
+    """
+    Load csv file and return a NumPy array of its data
+
+    Parameters
+    ----------
+    path: str
+        The path to the csv file to load
+    delimiter: str (default: ',')
+        The csv field delimiter
+
+    Return
+    ------
+    D: array
+        The NumPy array of the data contained in the file
+    """
+    return pd.read_csv(path, delimiter=delimiter).values.squeeze()
+
+def make_submission(y_predict, user_movie_ids, file_name='submission',
+                    date=True):
+    """
+    Write a submission file for the Kaggle platform
+
+    Parameters
+    ----------
+    y_predict: array [n_predictions]
+        The predictions to write in the file. `y_predict[i]` refer to the
+        user `user_ids[i]` and movie `movie_ids[i]`
+    user_movie_ids: array [n_predictions, 2]
+        if `u, m = user_movie_ids[i]` then `y_predict[i]` is the prediction
+        for user `u` and movie `m`
+    file_name: str or None (default: 'submission')
+        The path to the submission file to create (or override). If none is
+        provided, a default one will be used. Also note that the file extension
+        (.txt) will be appended to the file.
+    date: boolean (default: True)
+        Whether to append the date in the file name
+
+    Return
+    ------
+    file_name: path
+        The final path to the submission file
+    """
+
+    # Naming the file
+    if date:
+        file_name = '{}_{}'.format(file_name, time.strftime('%d-%m-%Y_%Hh%M'))
+
+    file_name = '{}.txt'.format(file_name)
+
+    # Writing into the file
+    with open(file_name, 'w') as handle:
+        handle.write('"USER_ID_MOVIE_ID","PREDICTED_RATING"\n')
+        for (user_id, movie_id), prediction in zip(user_movie_ids,
+                                                 y_predict):
+
+            if np.isnan(prediction):
+                raise ValueError('The prediction cannot be NaN')
+            line = '{:d}_{:d},{}\n'.format(int(user_id), int(movie_id), prediction)
+            handle.write(line)
+    return file_name
+
+
+data = load_from_csv("Lamborelle_Renaud_Vandegar_12-12-2018_13h32.csv")
+result = np.zeros(np.shape(data)[0])
+for i,d in enumerate(data):
+    if(d[1] >= 5):
+        d[1] = 5
+    if(d[1] <= 0):
+        d[1] = 0
+    result[i] = d[1]
+
+test_user_movie_pairs = load_from_csv('data/data_test.csv')
+fname = make_submission(result, test_user_movie_pairs, 'Lamborelle_Renaud_Vandegar')
+
